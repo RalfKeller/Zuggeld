4c3428b7791a0cbd699ec08e31cd43686c9311ff
Stefan Mayer <stefanma@uni-paderborn.de>
2018-04-13 17:38:05 +0200

    Finally finished that fucking shitty shit

--- CronJob/getitems.php
+++ CronJob/getitems.php
@@ -6,6 +6,15 @@ $DBPass   = 'maxdubaum';
 $DBName   = 'web137_db2';
 
 $conn = new mysqli($DBServer, $DBUser, $DBPass, $DBName);
+
+function turn_date($date) {
+
+    list($day,$month,$year) = explode(".",$date);
+    // echo "<br>". $date_array[2] . $date_array[1] .$date_array[0]."<br>";
+    $turn_date = $year .".". $month.".".$day;
+    return $turn_date;
+
+}
  
 // check connection
 if ($conn->connect_error) {
@@ -18,10 +27,13 @@ else{
 $json_jobs = shell_exec("curl -u 39d09354c0474b5ea187b4ef20931431: https://app.scrapinghub.com/api/jobs/list.json?project=269022&spider=db&state=finished&count=2");
 $jobs = json_decode($json_jobs);
 $id_array = array();
+$counter= 0;
 foreach($jobs->jobs as $job){
     array_push($id_array, $job->id);
+    $counter = $counter + 1;
 }
 
+echo "Found $counter Jobs!";
 $items_array_array = array();
 
 foreach($id_array as $id){
@@ -29,6 +41,7 @@ foreach($id_array as $id){
     array_push($items_array_array,$items);
 }
 
+$linecounter = 1;
 
 foreach($items_array_array as $json_item){
     
@@ -40,12 +53,12 @@ foreach($items_array_array as $json_item){
         $end_station = $item->end_station;
         $realtime = trim($item->realtime);
         $prognose = trim($item->prognose);
-        $datum = $item->datum;
-        $arrival_date = $item->arrival_date;
+        $datum = turn_date($item->datum);
+        $arrival_date = turn_date($item->arrival_date);
         $train_number = $item->train_number;
         $train_type = $item->train_type;
         $departure_time = $item->departure_time;
-        $departure_time = trim($departure_time);
+        
 
         if(is_null($train_number)){
             $train_number = "not found";
@@ -53,16 +66,42 @@ foreach($items_array_array as $json_item){
         if(is_null($train_type)){
             $train_type = "not found";
         }
-        if(is_null($realtime)){
+        if(empty($realtime)){
             $realtime = $prognose;
         }
+        echo "<br> Depart:".$departure_time[0]."<br>";
+        if(empty($departure_time[0])){
+            $departure_time=array(0 => "25:00:00");
+        }
+
+        $realtime = str_replace("\xc2\xa0",'',$realtime);
+        $prognose =  str_replace("\xc2\xa0",'',$prognose);
+        // $prognose = trim($prognose);
+        $departure_time = str_replace("\xc2\xa0",'',$departure_time);
+
+        
+        echo "<br>".$linecounter."<br>";
+        echo "Start Station: ".$start_station."<br>";
+        echo "End Station: ".$end_station."<br>";
+        echo "Real Time: ".$realtime."<br>";
+        echo "Type Real Time".var_dump($realtime)."<br>";
+        echo "Prognose: ".$prognose."<br>";
+        echo "Type Prognose".var_dump($prognose)."<br>";
+        echo "Encoding".mb_detect_encoding($prognose)."<br>";
+        echo "Datum: ".$datum."<br>";
+        echo "Arrival Date: ".$arrival_date."<br>";
+        echo "Train Number: ".$train_number."<br>";
+        echo "Train Type: ".$train_type."<br>";
+        echo "Departure Time: ".$departure_time."<br>";
+        echo "<br> <br>";
+        $linecounter = $linecounter + 1;
 
 
         if(!($stmt = $conn->prepare("INSERT INTO db_verspaetungen (start_bahnhof, ziel_bahnhof, echte_ankunft , ankunft, datum, datum_ankunft, zug_nummer, zug_typ, abfahrt) VALUES (?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE start_bahnhof = VALUES(start_bahnhof)"))){
             echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
         }
         else{
-            // echo "Prepare WIN!";
+            echo "Prepare WIN!";
         }
 
         // if(!$stmt){
@@ -73,21 +112,22 @@ foreach($items_array_array as $json_item){
             echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
         }
         else{
-            // echo "Binding WIN!";
+            echo "Binding WIN!";
         }
 
         if (!$stmt->execute()) {
+            // echo "\"".$prognose. "\"" . "<br>";
             echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-            echo <br>;
-            echo $prognose;
+            echo "<br>";
+            // echo $prognose;
         }
         else{
-            // echo "Execute WIN!";
+            echo "Execute WIN!";
         }
 
-        if($stmt->affected_rows === 0) exit('No rows updated');
+        if($stmt->affected_rows === 0) echo ('No rows updated');
         else{
-            // echo "Rows affected:" . $stmt->affected_rows;
+            echo "Rows affected:" . $stmt->affected_rows;
         }
 
 
@@ -112,11 +152,13 @@ foreach($items_array_array as $json_item){
         //     echo $last_inserted_id . "\n";
         //     echo $affected_rows . "\n";
         //   }
+        $stmt->close();
     }
 
 
 }
 
+$conn->close();
 
 // file_put_contents("text.json",$jobs);
 ?>
